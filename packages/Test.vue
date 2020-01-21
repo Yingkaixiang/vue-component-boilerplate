26d0dbbf66b402d2f205e58ba74e7bc89c79a283
应开翔 <yingkaixiang@qutoutiao.net>
2020-01-21 15:07:34 +0800

    feat: add storybook

--- packages/Test.vue
+++ packages/Test.vue
@@ -1,29 +1,13 @@
 <template>
-  <div :class="$a.foo">
-    {{ msg }}
-    <img src="./pokeball.png">
-  </div>
+  <div>{{ msg }}</div>
 </template>
 
 <script>
-import Test1 from '@/Test1';
-
 export default {
   data () {
     return {
-      msg: 'Hello world!'
+      msg: 'Hello world!222'
     }
   },
-  
-  methods: {
-    test() {
-      const a = 1;
-    }
-  }
 }
 </script>
-
-<style lang="scss" module="a">
-.foo { color: $color }
-.bar { display: flex }
-</style>
